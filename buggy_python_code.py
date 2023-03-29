0e7bd4fe98eb20e42573ff306143d5cedd757f4a
xkonsta1 <129223154+xkonsta1@users.noreply.github.com>
2023-03-29 09:06:11 +0200

    syntax fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -23,7 +23,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    # exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
     try:
